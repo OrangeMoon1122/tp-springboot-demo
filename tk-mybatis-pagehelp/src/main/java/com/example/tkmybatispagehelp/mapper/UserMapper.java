337c144f51bb0a18c0efa8154f5be4c81f593e85
OrangeMoon1122 <wangzhihang999@gmail.com>
2020-04-04 22:19:38 +0800

    fix:添加service描扫，删除无用包

--- tk-mybatis-pagehelp/src/main/java/com/example/tkmybatispagehelp/mapper/UserMapper.java
+++ tk-mybatis-pagehelp/src/main/java/com/example/tkmybatispagehelp/mapper/UserMapper.java
@@ -2,11 +2,14 @@ package com.example.tkmybatispagehelp.mapper;
 
 import com.example.tkmybatispagehelp.entity.User;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
 /**
  * @author wangz
  */
+
+@Component("userMapper")
 public interface UserMapper extends Mapper<User>, MySqlMapper<User> {
 }
