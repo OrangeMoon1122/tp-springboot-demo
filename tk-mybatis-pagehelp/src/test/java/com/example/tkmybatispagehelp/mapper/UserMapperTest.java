bcd6e24ab880533869273ce7d144ecc7106b5485
OrangeMoon1122 <wangzhihang999@gmail.com>
2020-04-05 09:37:55 +0800

    fix

--- tk-mybatis-pagehelp/src/test/java/com/example/tkmybatispagehelp/mapper/UserMapperTest.java
+++ tk-mybatis-pagehelp/src/test/java/com/example/tkmybatispagehelp/mapper/UserMapperTest.java
@@ -3,6 +3,7 @@ package com.example.tkmybatispagehelp.mapper;
 import com.example.tkmybatispagehelp.TkMybatisPagehelpApplication;
 import com.example.tkmybatispagehelp.TkMybatisPagehelpApplicationTests;
 import com.example.tkmybatispagehelp.entity.User;
+import com.sun.deploy.association.utility.WinRegistryUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.javassist.bytecode.stackmap.BasicBlock;
 import org.junit.Assert;
@@ -34,43 +35,48 @@ public class UserMapperTest extends TkMybatisPagehelpApplicationTests {
                 .password("123")
                 .email("123@email.com")
                 .phoneNumber("133161")
-                .status(1)
-                .lastLoginTime(null)
+
                 .createTime(new Date())
                 .lastUpdateTime(new Date())
                 .build();
 
+        // 测试保存部分参数
         try {
-          int res =  userMapper.insertSelective(user);
-            log.info("保存单个实体，自动生成主键并返回{}", res);
+            int res = userMapper.insertSelective(user);
+            log.info("保存单个实体，自动生成主键并返回受影响的条数 {} ", res);
         } catch (Exception e) {
-            log.info("保存单个实体，出现异常{}", e.getMessage());
+            log.error("保存单个实体，出现异常{}", e.getMessage());
         }
 
+        // 测试保存一个实体
+        // 数据库设置自增主键的时候，如果当前数据库主键最大 5 实体自定义了主键 100，
+        // 那么会选择实体的主键，且下次从当前最大主键继续增加为 101
         try {
             user.setSalt("SALT");
+            user.setStatus(1);
+            user.setLastLoginTime(null);
+            user.setId(100L);
             userMapper.insert(user);
             log.info("保存单个实体，自动生成主键并返回{}", user.getId());
         } catch (Exception e) {
-            log.info("保存单个实体，出现异常{}", e.getMessage());
+            log.error("保存单个实体，出现异常{}", e.getMessage());
         }
 
+        // 测试保存自动生成id，插入选择的属性
+        try {
+            userMapper.insertUseGeneratedKeys(user);
+            log.info("保存单个实体，自动生成主键并返回{}", user.getId());
+        } catch (Exception e) {
+            log.error("保存单个实体，出现异常{}", e.getMessage());
+        }
 
-//
-//
-//        try {
-//            userMapper.insertUseGeneratedKeys(user);
-//            log.info("保存单个实体，自动生成主键并返回{}", user.getId());
-//        } catch (Exception e) {
-//            log.info("保存单个实体，出现异常{}", e.getMessage());
-//        }
-//
+        // 测试保存自动设置的id
         try {
             user.setId(user.getId() + 10);
             userMapper.insertUseGeneratedKeys(user);
             log.info("保存单个实体，自动生成主键并返回{}", user.getId());
         } catch (Exception e) {
-            log.info("保存单个实体，出现异常{}", e.getMessage());
+            log.error("保存单个实体，出现异常{}", e.getMessage());
         }
     }
 
@@ -78,7 +84,18 @@ public class UserMapperTest extends TkMybatisPagehelpApplicationTests {
      * 单表的更新：
      * 重点如果只是修改部分参数如何更新呢？
      */
+    @Test
     public void UpdateUser() {
+
+        User user = userMapper.selectByPrimaryKey(100);
+        user.setEmail("测试更新");
+
+        try {
+            userMapper.updateByPrimaryKey(user);
+            log.info("更新单个实体成功");
+        }catch (Exception e){
+            log.error("更新单个实体{}",e.getMessage());
+        }
 
     }
 }
